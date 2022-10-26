65bc39d7f773e14c29cac1d1a8ec15201bf3bd6f
Th3c0d3d1 <bittradeguy007@gmail.com>
2022-10-26 00:07:31 +0000

    functional light

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,51 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
+
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
-const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+// const Home = () => {
+// 	return (
+// 		<div className="text-center">
+// 			<h1 className="text-center mt-5">Hello Rigo!</h1>
+// 			<p>
+// 				<img src={rigoImage} />
+// 			</p>
+// 			<a href="#" className="btn btn-success">
+// 				If you see this green button... bootstrap is working...
+// 			</a>
+// 			<p>
+// 				Made by{" "}
+// 				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
+// 				love!
+// 			</p>
+// 		</div>
+// 	);
+// };
+
+export function Home(){
+	const[selectedColor, setSelectedColor] = useState("");
+	
+	return(
+		<div container class="text-center">
+			<div className="traffic-light rounded">
+				<div 
+					onClick={() => setSelectedColor("red")}
+					className={
+					"red light" + ((selectedColor === "red") ? " glow" : "")}>
+				</div>
+				<div 
+					onClick={() => setSelectedColor("yellow")}
+					className={"yellow light" + ((selectedColor === "yellow") ? " glow" : "")}>
+				</div>
+				<div 
+					onClick={() => setSelectedColor("green")}
+					className={"green light" + ((selectedColor === "green") ? " glow" : "")}>
+				</div>
+			</div>
 		</div>
-	);
-};
+	)
+}
 
 export default Home;
--- src/js/index.js
+++ src/js/index.js
@@ -3,6 +3,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 // include your styles into the webpack bundle
+import 'bootstrap';
 import "../styles/index.css";
 
 //import your own components
